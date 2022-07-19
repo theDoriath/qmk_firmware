fff89a1def9bd49ec007a0af8522a476595adeb2
Robin <robin@robinmorgan.nl>
2022-07-19 17:07:13 +0200

    disabled tap dance

--- keyboards/gmmk/gmmk2/p65/ansi/rules.mk
+++ keyboards/gmmk/gmmk2/p65/ansi/rules.mk
@@ -7,8 +7,8 @@ BOOTLOADER = wb32-dfu
 # Build Options
 #   change yes to no to disable
 #
-TAP_DANCE_ENABLE=yes
-LEADER_ENABLE = yes
+TAP_DANCE_ENABLE=no
+LEADER_ENABLE = no
 BOOTMAGIC_ENABLE = yes       # Enable Bootmagic Lite.
 MOUSEKEY_ENABLE = yes        # Mouse keys.
 EXTRAKEY_ENABLE = yes        # Audio control and System control.
