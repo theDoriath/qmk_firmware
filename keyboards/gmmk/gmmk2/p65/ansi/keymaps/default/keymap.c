313d04f057ef06e50851d970a99e81ecc5bb1d16
Robin <robin@robinmorgan.nl>
2022-07-19 18:19:42 +0200

    fdsfdsf

--- keyboards/gmmk/gmmk2/p65/ansi/keymaps/default/keymap.c
+++ keyboards/gmmk/gmmk2/p65/ansi/keymaps/default/keymap.c
@@ -5,43 +5,43 @@
 
 
 enum custom_keycodes {
-    COPY = 0,
-	PASTE,
-	CUT,
-	UNDO
+    XXCOPY = 0,
+	XXPASTE,
+	XXCUT,
+	XXUNDO
 };
 
 
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
-    case COPY:
+    case XXCOPY:
         if (record->event.pressed) {
             // when keycode QMKBEST is pressed
-             SEND_STRING(SS_LCTL("ac"));
+             SEND_STRING("coy");
         } else {
             // when keycode QMKBEST is released
         }
         break;
-	case PASTE:
+	case XXPASTE:
         if (record->event.pressed) {
             // when keycode QMKBEST is pressed
-            SEND_STRING(SS_LCTL("V")); 
+            SEND_STRING("paste"); 
         } else {
             // when keycode QMKBEST is released
         }
         break;
-	case CUT:
+	case XXCUT:
         if (record->event.pressed) {
             // when keycode QMKBEST is pressed
-           SEND_STRING(SS_LCTL("X")); 
+           SEND_STRING(SS_LCTL("x")); 
         } else {
             // when keycode QMKBEST is released
         }
         break;
-	case UNDO:
+	case XXUNDO:
         if (record->event.pressed) {
-            // when keycode QMKBEST is pressed
+            // when keycode QMKBEST is fzz
             SEND_STRING(SS_LCTL("Z")); 
         } else {
            
@@ -51,25 +51,27 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     return true;
 };
 
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [0] = LAYOUT(
-KC_GESC, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_EQL, KC_BSPC, RGB_TOG,
-KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC, KC_BSLS, KC_PGUP,
-MO(1), KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_ENT, KC_PGDN,
-KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT, KC_UP, KC_DEL,
-KC_LCTL, KC_LGUI, KC_LALT, KC_SPC, KC_RALT, MO(1), KC_LEFT, KC_DOWN, KC_RGHT),
+  KC_GESC,  KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,  KC_EQL,   KC_BSPC,  KC_DEL,
+  KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,  KC_RBRC,  KC_BSLS,   KC_PGUP,
+  TO(1),  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,  KC_ENT,            KC_PGDN,
+  KC_LSFT,  KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  KC_RSFT,  KC_UP,    KC_END,
+  KC_LCTL,  KC_LGUI,  KC_LALT,                                KC_SPC,                                 KC_RALT,  MO(1),    KC_LEFT,  KC_DOWN,  KC_RGHT),
 
 [1] = LAYOUT(
-KC_GRV, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, _______, RGB_MOD,
-_______, _______, _______, KC_BSPC, _______, _______, _______, _______, _______, _______, KC_CAPS, RGB_HUD, RGB_HUI, _______, RGB_VAI,
-_______, KC_WREF, _______, KC_DEL, RGB_SPD, RGB_SPI, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, KC_END, _______, RGB_VAD,
-_______, _______, UNDO, CUT, COPY, PASTE, RGB_VAD, RGB_VAI, KC_HOME, KC_VOLD, KC_VOLU, KC_MUTE, _______, KC_PGUP, KC_INS,
-_______, _______, _______, RESET, _______, _______, KC_HOME, KC_PGDN, KC_END),
+   KC_GRV,    KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,   KC_F10,   KC_F11,   KC_F12,   _______,  KC_INS,
+  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  KC_PSCR,  KC_SCRL,  KC_PAUS,   _______,  _______,
+  TO(0),  RGB_HUI,  RGB_HUD,  RGB_SPD,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______, _______,  _______,  
+  _______,  XXUNDO,  XXCUT,  XXCOPY,  XXPASTE,  KC_MUTE,  KC_VOLU,  KC_VOLD,  KC_MPRV,  KC_MPLY,  KC_MNXT,  _______,   RGB_VAI,  KC_HOME,
+  _______,  _______,  _______,                                RESET,                                  _______,  _______,  RGB_RMOD,  RGB_VAD,  RGB_MOD),
 
 [2] = LAYOUT(
-_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-_______, _______, _______, _______, _______, _______, _______, _______, _______)
+  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,   _______,  _______,
+  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,   _______,  _______,
+  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,             _______,
+  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,   _______,  _______,
+  _______,  _______,  _______,                                _______,                                _______,  _______,  _______,   _______,  _______)
 };
+
