4d8187180fefae4e6f7e92d3850b9c8099ae553d
Robin <robin@robinmorgan.nl>
2022-07-19 20:23:22 +0200

    setting some more defaults back

--- keyboards/gmmk/gmmk2/p65/ansi/rules.mk
+++ keyboards/gmmk/gmmk2/p65/ansi/rules.mk
@@ -7,8 +7,6 @@ BOOTLOADER = wb32-dfu
 # Build Options
 #   change yes to no to disable
 #
-TAP_DANCE_ENABLE=no
-LEADER_ENABLE = no
 BOOTMAGIC_ENABLE = yes       # Enable Bootmagic Lite.
 MOUSEKEY_ENABLE = yes        # Mouse keys.
 EXTRAKEY_ENABLE = yes        # Audio control and System control.
