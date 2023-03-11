a44d5246d3863dbc32f9acfc9548329e199d0ab6
doriath <93petter93@gmail.com>
2023-03-11 23:40:21 +0100

    remove capslock lock toggle and remap casplock to F13 on layer 0

--- keyboards/gmmk/gmmk2/p65/ansi/keymaps/doriath/keymap.c
+++ keyboards/gmmk/gmmk2/p65/ansi/keymaps/doriath/keymap.c
@@ -37,14 +37,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [0] = LAYOUT(
   KC_ESC,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,  KC_EQL,   KC_BSPC,  KC_DEL,     // 00-14
   KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,  KC_RBRC,  KC_BSLS,  KC_HOME,    // 15-29
-  KC_CAPS,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,  KC_ENT,             KC_END,     // 30-43
+  KC_F13,   KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,  KC_ENT,             KC_END,     // 30-43
   KC_LSFT,  KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  KC_RSFT,            KC_UP,    KC_PGDN,    // 44-57
   KC_LCTL,  KC_LGUI,  KC_LALT,                                KC_SPC,                                 KC_RALT,  MO(1),    KC_LEFT,  KC_DOWN,  KC_RGHT),   // 58-66
 
 [1] = LAYOUT(
   KC_GRV,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,   _______,  KC_INS,
   _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  KC_PSCR,  KC_SCRL,  KC_PAUS,  MD_BOOT,  _______,
-  MD_LCAP,  _______,  RGB_TOG,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,            _______,
+  KC_CAPS,  _______,  RGB_TOG,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,            _______,
   _______,  RGB_HUI,  RGB_HUD,  RGB_SPD,  RGB_SPI,  KC_MUTE,  KC_VOLD,  KC_VOLU,  KC_MPRV,  KC_MPLY,  KC_MNXT,  _______,            RGB_VAI,  KC_PGUP,
   _______,  GUI_TOG,  _______,                                _______,                                TG(2),    _______,  RGB_RMOD, RGB_VAD,  RGB_MOD),
 
@@ -115,18 +115,19 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   switch (keycode) {
   case KC_CAPS:
       if (record->event.pressed) {
-        if (caps_lock_on) {
-          unregister_code(KC_CAPS);
-        } else {
-          register_code(KC_CAPS);
-        }
+        // if (caps_lock_on) {
+        //   unregister_code(KC_CAPS);
+        // } else {
+        //   register_code(KC_CAPS);
+        // }
+        caps_lock_on = !caps_lock_on;
       }
-      return false;
-  case MD_LCAP:
-    if (record->event.pressed) {
-      caps_lock_on = !caps_lock_on;
-    }
-    return false;
+      return true;
+  // case MD_LCAP:
+  //   if (record->event.pressed) {
+  //     caps_lock_on = !caps_lock_on;
+  //   }
+  //   return false;
 
 
   // case KC_LGUI:
@@ -258,12 +259,12 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 
 
 void suspend_power_down_user(void) {
-    rgb_matrix_set_suspend_state(true);
+    // rgb_matrix_set_suspend_state(true);
 }
 
 void suspend_wakeup_init_user(void) {
-    rgb_matrix_set_suspend_state(false);
-    clear_mods();
+    // rgb_matrix_set_suspend_state(false);
+    // clear_mods();
 }
 
 
